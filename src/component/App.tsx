51024ad492508cdac47d25521aab5e0ec2e08660
Rémi Lelaidier <r.lelaidier@hotmail.fr>
2018-03-28 15:15:00 +0200

    Remove Log

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -31,7 +31,6 @@ class App extends React.Component {
   }
 
   onMenuButtonClick = (event: any) => {
-    console.log('clicked')
     this.setState((prevState: AppState) => {
       return {
         dialogs: {
